db745aa1a51f18909fb1209ae5270511a45d9fc4
Markus grit:lab <markus.amberla@gmail.com>
2025-05-06 13:10:55 +0300

    pinia store to composition style

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -1,19 +1,24 @@
+// stores/user.js
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useUserStore = defineStore('user', {
-    state: () => ({
-        user: null
-    }),
-    actions: {
-        setUser(userData) {
-            console.log("This user:", userData)
-            this.user = userData
-        },
-        clearUser() {
-            this.user = null
-        }
-    },
-    getters: {
-        isLoggedIn: (state) => !!state.user
+export const useUserStore = defineStore('user', () => {
+    const user = ref(null)
+
+    const setUser = (userData) => {
+        user.value = userData
+    }
+
+    const clearUser = () => {
+        user.value = null
+    }
+
+    const isLoggedIn = computed(() => !!user.value)
+
+    return {
+        user,
+        setUser,
+        clearUser,
+        isLoggedIn
     }
 })
