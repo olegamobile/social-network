64cde99f011b4225a0679d908a75938b62a17889
kovsiien <ketio403@gmail.com>
2025-05-22 17:56:50 +0300

    events frontend

--- frontend/src/components/EventCard.vue
+++ frontend/src/components/EventCard.vue
@@ -1,12 +1,37 @@
 <template>
     <div class="event-card">
+        <div class="bg-white p-6 rounded-lg shadow-md">
         <h2 class="text-2xl font-bold mb-4">{{ event.title }}</h2>
-        <p><strong>Group:</strong> {{ event.group }}</p>
-        <p><strong>Time:</strong> {{ event.time }}</p>
-        <p><strong>Description:</strong> {{ event.description }}</p>
-        <p><strong>Going:</strong> {{ event.going.join(', ') }}</p>
-        <button>Going</button>
-        <button>Not Going</button>
+
+        <div class="space-y-2">
+          
+        <p>
+           <span class="font-semibold text-nordic-dark">Group:</span>
+           <span class="text-nordic-light ml-1">{{ event.group }}</span> 
+        </p>
+        <p>
+            <span class="font-semibold text-nordic-dark">Time:</span> 
+            <span class="text-nordic-light ml-1">{{ event.time }}</span>
+        </p>
+        <p>
+            <span class="font-semibold text-nordic-dark">Description:</span> 
+            <span class="text-nordic-light ml-1">{{ event.description }}</span>
+        </p>
+        <p>
+            <span class="font-semibold text-nordic-dark">Going:</span> 
+            <span class="text-nordic-light ml-1">{{ event.going.join(', ') }}</span>
+        </p>
+        </div>
+        <!-- <p><strong>Not Going:</strong> {{ event.notGoing.join(', ') }}</p>
+        <p><strong>Pending:</strong> {{ event.pending.join(', ') }}</p> -->
+
+        <div class="flex flex-row gap-4 mt-4">
+        <button type="button" @click="$emit('edit', event.id)"
+        class="bg-nordic-primary-accent text-white font-medium px-5 py-2 rounded-md hover:bg-nordic-secondary-accent transition">Going</button>
+        <button type="button" @click="$emit('notGoing', event.id)"
+        class="bg-nordic-primary-accent text-white font-medium px-5 py-2 rounded-md hover:bg-nordic-secondary-accent transition">Not Going</button>
+        </div>
+    </div>
     </div>
 </template>
 
