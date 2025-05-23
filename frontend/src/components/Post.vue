4a569918954a638fb4b27112b0371065ad89c705
Markus grit:lab <markus.amberla@gmail.com>
2025-05-23 15:29:27 +0300

    nordic colors to components

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,24 +1,36 @@
 <template>
-    <div class="post-card">
-        <img v-if="post.image_path" :src="`${apiUrl}/${post.image_path}`" alt="">
-        <p class="post-content">{{ post.content }}</p>
-        <small class="post-date flex items-center">
-            <RouterLink :to="`/profile/${post.user_id}`" class="post-user flex items-center mr-1">
-                <div v-if="post.avatar_url" class="post-user-avatar w-6 h-6 rounded-full overflow-hidden mr-1">
+    <div
+        class="post-card bg-[var(--nordic-primary-bg)] border border-[var(--nordic-border-light)] rounded-md p-4 shadow-sm space-y-3">
+        <img v-if="post.image_path" :src="`${apiUrl}/${post.image_path}`" alt=""
+            class="w-full rounded-md border border-[var(--nordic-border-light)]" />
+
+        <p class="post-content text-[var(--nordic-text-dark)] text-base">
+            {{ post.content }}
+        </p>
+
+        <small class="post-date flex items-center text-sm text-[var(--nordic-text-light)]">
+            <RouterLink :to="`/profile/${post.user_id}`"
+                class="post-user flex items-center mr-1 hover:underline text-[var(--nordic-text-dark)]">
+                <div v-if="post.avatar_url"
+                    class="post-user-avatar w-6 h-6 rounded-full overflow-hidden mr-1 border border-[var(--nordic-border-light)]">
                     <img :src="`${apiUrl}/${post.avatar_url}`" alt="User Avatar" class="w-full h-full object-cover" />
                 </div>
                 {{ post.username }}
             </RouterLink>
-            <span v-if="post.group_id">
+
+            <span v-if="post.group_id" class="text-[var(--nordic-text-light)]">
                 in
-                <RouterLink :to="`/group/${post.group_id}`" class="text-blue-500 hover:underline mr-1">
+                <RouterLink :to="`/group/${post.group_id}`"
+                    class="text-[var(--nordic-primary-accent)] hover:underline mr-1">
                     {{ post.group_name }}
                 </RouterLink>
             </span>
+
             on {{ formattedDate }}
         </small>
     </div>
 </template>
+
 
 <script setup>
 import { computed } from 'vue'
